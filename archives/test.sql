00da1d8f853e4d7f76df328a4d26d96e380bc128
swrangsar basumatary <swrangsar@example.com>
2014-06-19 11:23:23 +0530

    commit at 20140619112323

--- archives/test.sql
+++ archives/test.sql
@@ -1,2 +1,2 @@
-INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404070411411137', '2222', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404070411411137');
-INSERT INTO dialdata_table (exten, dial) VALUES ('2222', 'IMSI404070411411137');
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404201572311737', '5555', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404201572311737');
+INSERT INTO dialdata_table (exten, dial) VALUES ('5555', 'IMSI404201572311737');
