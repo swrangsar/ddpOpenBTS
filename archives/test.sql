da27d69bf9337eed445badb61be1ed1cda320c2f
swrangsar basumatary <swrangsar@iitb.ac.in>
2014-06-21 16:34:23 +0530

    commit at 20140621163423

--- archives/test.sql
+++ archives/test.sql
@@ -1,2 +1,11 @@
-INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404201572311737', '5555', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404201572311737');
-INSERT INTO dialdata_table (exten, dial) VALUES ('5555', 'IMSI404201572311737');
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404070411411137', '5555', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404070411411137');
+INSERT INTO dialdata_table (exten, dial) VALUES ('5555', 'IMSI404070411411137');
+
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404929227967046', '9999', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404929227967046');
+INSERT INTO dialdata_table (exten, dial) VALUES ('9999', 'IMSI404929227967046');
+
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404929509230948', '7777', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404929509230948');
+INSERT INTO dialdata_table (exten, dial) VALUES ('7777', 'IMSI404929509230948');
+
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404201572311737', '2222', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404201572311737');
+INSERT INTO dialdata_table (exten, dial) VALUES ('2222', 'IMSI404201572311737');
